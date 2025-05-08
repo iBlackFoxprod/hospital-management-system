bf2619c3f545b010c32f170c30adba98a75fd2f6
alisaadaoui <alisaadaoui6@email.com>
2025-05-08 16:04:18 +0000

    removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'firebase_options.dart';
 import 'services/auth_service.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
